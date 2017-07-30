5dee9dfc7386932932deb502dfbc8de8e591b6bf
tichimactichi <tichirotich@gmail.com>
2017-07-30 20:31:57 +0300

    commit message

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main (){
+
+    int first, second;
+
+    cout << "Please enter two integers." << endl;
+
+    cout << "First" << setw (3) << ": ";
+    cin >> first;
+
+    cout << "Second" << setw (2) << ": ";
+    cin >> second;
+
+    string message = first > second ? "first is greater than second" : "first is less than or equal to second";
+
+    cout << message << endl;
+    return 0;
+}
